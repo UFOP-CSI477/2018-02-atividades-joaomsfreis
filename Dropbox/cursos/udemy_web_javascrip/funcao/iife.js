f7af9556dc4283bb33ece0b30505137dccf493a3
joaomsfreis <joaomsfreis@gmail.com>
2019-04-15 23:00:34 -0300

    teste

---
+++ Dropbox/cursos/udemy_web_javascrip/funcao/iife.js
@@ -0,0 +1,8 @@
+//IIFE -> Imediately Invoked Function Expression
+
+(function(){
+    console.log('Será executada na hora!')
+    console.log('Foge do escopo mais abrangente!')
+})()
+
+console.log('Aqui vai o teste...')
